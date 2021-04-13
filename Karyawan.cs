1b735ff6c0ed684129d7c2c32702322031e09749
raditya <radityabagaswara@students.amikom.ac.id>
2021-04-13 12:29:53 +0700

    menambahkan tugas responsi

---
+++ Karyawan.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman3352
+{
+    class Karyawan
+    {
+        public string Nama { get; set; }
+        public string NIK { get; set; }
+        public int Gaji { get; set; }
+        public int Naik { get; set; }
+
+        public Karyawan (string nama ,string nik ,int gaji){
+            this.NIK = nik;
+            this.Nama = nama;
+            if (gaji < 0){
+                this.Gaji = 0;
+                Naik = Convert.ToInt32((Gaji * 1.1));
+            }else{
+                this.Gaji = gaji;
+                Naik = Convert.ToInt32((Gaji * 1.1));
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman3352
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Karyawan karyawan1 = new Karyawan("Sutarman", "20113352", 5000000);
+            Karyawan karyawan2 = new Karyawan("Paidi", "20113315", -100);
+
+            Console.WriteLine("NO   NIK / NAMA\t\t          Gaji ");
+            Console.WriteLine("------------------------------------------");
+            Console.WriteLine("1. {0} {1}\t {2}", karyawan1.NIK, karyawan1.Nama, karyawan1.Gaji);
+            Console.WriteLine("2. {0} {1}\t {2}\t ", karyawan2.NIK, karyawan2.Nama, karyawan2.Gaji);
+            Console.WriteLine();
+            Console.WriteLine("Alhamdulillah Naik Gaji 10%");
+            Console.WriteLine();
+            Console.WriteLine("NO   NIK / NAMA\t\t          Gaji ");
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("1. {0} {1}\t {2}", karyawan1.NIK, karyawan1.Nama, karyawan1.Naik);
+            Console.WriteLine("2. {0} {1}\t {2}\t ", karyawan2.NIK, karyawan2.Nama, karyawan2.Naik);
+
+            Console.ReadKey();
+        }
+    }
+}
